27b97730caca802f2c33a19888760fbab21a7ff0
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-09-19 19:53:00 +0530

    Update settings.py

--- shopwebsite/settings.py
+++ shopwebsite/settings.py
@@ -129,7 +129,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # React frontend
+    "https://shoewebfront.vercel.app/",  # React frontend
 ]
 
 REST_FRAMEWORK = {
