009080cdc6adf579ee908669c6021ef06671ca28
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-09-21 12:10:26 +0530

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,18 @@
-from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer  # create this serializer
 
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
 
