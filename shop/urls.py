acefc75f41d35385d9bfaf7b47a88a97816f36bc
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-09-21 12:07:59 +0530

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,15 +1,11 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-router = DefaultRouter()
-router.register(r'products', ProductViewSet, basename='product')
+from .views import RegisterView  # custom view
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
 
 
